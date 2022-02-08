ac69e66ecfc06dc95a8a21135df7b8cb233d622a
laura <laura@Laura>
2022-02-08 11:41:38 +0100

    crear enum tramo completo

---
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/modelo/dominio/Tramo.java
@@ -0,0 +1,16 @@
+package org.iesalandalus.programacion.reservasaulas.mvc.modelo.dominio;
+
+public enum Tramo {
+	MANANA("Mañana"), TARDE("Tarde");
+
+	String cadenaAMostrar;
+
+	private Tramo(String cadenaAMostrar) {
+		this.cadenaAMostrar = cadenaAMostrar;
+	}
+
+	public String toString() {
+		return cadenaAMostrar;
+	}
+
+}
